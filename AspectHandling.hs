5bad128f8306f2669a4eaf1c04c7bca0a5ae69f5
accxev <antonia.schmalstieg@googlemail.com>
2016-09-03 16:08:58 +0200

    Aspect handling now supports more than 1 tag

--- AspectHandling.hs
+++ AspectHandling.hs
@@ -1,4 +1,6 @@
 import System.IO
+import Data.List.Split
+import Data.List
 
 main = do  
     -- handle <- openFile "aspects.txt" WriteMode
@@ -6,16 +8,24 @@ main = do
     putStrLn "This is a small inline-tool to add aspects to our aspect data base. Make sure they are meaningful as there is currently no option to delete them.\n"  
     putStrLn "So, what aspect do you want to add?"  
     aspect <- getLine
-    putStrLn ("Inserted aspect: \"" ++ aspect ++ "\", is that right?") -- If so, type Yes.")
+    -- putStrLn ("Inserted aspect: \"" ++ aspect ++ "\", is that right?") -- If so, type Yes.")
     -- answer <- getLine 
     putStrLn ("Now you need to add tags. Seperate them via commas.")
     tags <- getLine
-    putStrLn ("You added the following tags: " ++ tags)
+    -- putStrLn ("You added the following tags: " ++ tags)
 
+    appendFile "aspects.txt" ("\n\n- name: \"" ++ aspect ++"\"\n") -- ++ "\"\n  tags:\n\t- " ++ tags)
 
-    appendFile "aspects.txt" ("\n\n- name: \"" ++ aspect ++ "\"\n  tags:\n\t- " ++ tags)
+    let taglist = splitOn "," tags
+        new_taglist = map ("- " ++) taglist
+        tag_string = "\t" ++ intercalate "\n\t" new_taglist
 
+    appendFile "aspects.txt" tag_string
+     
     -- hClose handle 
+
+
+    
 
 
 
